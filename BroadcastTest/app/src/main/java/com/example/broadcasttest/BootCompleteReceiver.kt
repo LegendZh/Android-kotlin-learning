b0dd66eb697300b3f092424624a59b12575d09fd
“LeLgegendZh <407819082@qq.com>
2021-03-01 22:47:52 +0800

    20210301_6.2.2

---
+++ BroadcastTest/app/src/main/java/com/example/broadcasttest/BootCompleteReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.broadcasttest
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class BootCompleteReceiver : BroadcastReceiver() {
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Toast.makeText(context,"Boot Complete",Toast.LENGTH_LONG).show()
+    }
+}
